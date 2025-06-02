672099e95131064922a883365fa70b2155505ab8
ompa <nmaladina@hotmail.com>
2025-06-02 22:10:34 +1200

    adding component and reusable Input component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import NewProject from "./components/NewProject";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
   return (
-    < main className="h-screen my-8">
+    < main className="flex gap-8 h-screen my-8">
       <ProjectsSidebar />
+      <NewProject />
     </main>
   )
 }
