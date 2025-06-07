1e94a207b6ffdcfa0d9f3091f6639fa314c01162
ompa <nmaladina@hotmail.com>
2025-06-07 14:20:25 +1200

    Splitting Components to Split JSX & Tailwind Styles( for Higher Reusability)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
   return (
     < main className="flex gap-8 h-screen my-8">
       <ProjectsSidebar />
-      <NewProject />
+      <NoProjectSelected />
     </main>
   )
 }
