50ea58105f42c8804897baaa68de184d5ba8937a
ompa <nmaladina@hotmail.com>
2025-06-07 15:34:40 +1200

    Managing Stat to Switch Between Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
+import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
+import { useState } from "react";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+  
+  function handleStartProjectCreation() {
+    setProjectsState((prevState) => ({
+      ...prevState,
+      selectedProjectId: null,
+    }));
+  }
+  
+  let content;
+  
+  if(projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  }
+  else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartProjectCreation} />;
+  }
+  
   return (
     < main className="flex gap-8 h-screen my-8">
-      <ProjectsSidebar />
-      <NoProjectSelected />
+      <ProjectsSidebar onStartAddProject={handleStartProjectCreation}/>
+      {content}
     </main>
   )
 }
